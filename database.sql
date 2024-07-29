91ebceff115e0302b1b7ecd41736172db31b2deb
Bunnyeah <noobbunny2007@gmail.com>
2024-07-29 11:54:35 -0300

    massagem bonita

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE teste;
+USE teste;
+
+CREATE TABLE usuario (
+    ID          INT NOT NULL AUTO_INCREMENT,
+    NOME        VARCHAR(50),
+    EMAIL       VARCHAR(50),
+    TEL         VARCHAR(22)
+)
