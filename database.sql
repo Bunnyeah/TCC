13529f175fb4686fb82e308f241ee8a86d89258c
Mr Bunny <125466146+Bunnyeah@users.noreply.github.com>
2024-08-14 07:58:15 -0300

    Delete database.sql

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE teste;
-USE teste;
-
-CREATE TABLE usuario (
-    ID          INT NOT NULL AUTO_INCREMENT,
-    NOME        VARCHAR(50),
-    EMAIL       VARCHAR(50),
-    TEL         VARCHAR(22)
-)
