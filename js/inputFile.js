1cb5323527ea42dcb5aecb8ba6bff97efd0ae7d2
Mr Bunny <125466146+Bunnyeah@users.noreply.github.com>
2024-10-02 17:57:36 -0300

    Delete js directory

--- js/inputFile.js
+++
@@ -1,6 +0,0 @@
-    const button = document.getElementById("inputFile");
-    const inputFile = document.querySelector("[type=file]");
-    button.addEventListener("click", () => {
-        // Simula um clique no input file
-        inputFile.click()
-    })
--- js/mobileNavbar.js
+++
@@ -1,5 +0,0 @@
-    var x = document.getElementById("mobileNavbar");
-    var y = document.getElementById("navbar")
-    x.addEventListener("click", () =>{
-            y.classList.toggle("aberto")
-    });
