791e1eac65427a91cb521f280f648456e5517c7c
Nico-las1 <nicolasmoro013@gmail.com>
2024-10-18 10:43:11 -0300

    sla

--- user/historico.php
+++ user/historico.php
@@ -54,8 +54,8 @@ if ($status !== '') {
                 <div class="linha"><div class="seta"></div><a class="botao" href="alterarsenha.php"><p>Trocar Senha</p></a></div>
                 <div class="linha"><div class="seta"></div><a class="botao pagatual" href="./historico.php"><p>Histórico</p></a></div>
             </div>
-            <div id="sair"><a href="./config/logout.php"><p>Sair</p></a></div>
-        </div>
+                <div id="sair"><a href="../config/logout.php"><img src="../assets/imgs/icons/logoutbranco.svg"><p>Sair</p></a></div>
+            </div>
     </nav>
 
     <!-- Titulo -->
