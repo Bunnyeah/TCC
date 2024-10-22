549ef503d5881b8b3887227c2fa6a1578f749b5b
Nico-las1 <nicolasmoro013@gmail.com>
2024-10-22 08:20:18 -0300

    pronto

--- homepage.php
+++ homepage.php
@@ -12,6 +12,14 @@ $produtos   = $stmt->fetchAll(PDO::FETCH_OBJ);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./assets/css/style.css">
     <title>Home Page</title>
+    <style>
+      .btn-primary{
+        background: #133A41;
+      }
+      .btn-primary:active, .btn-primary:hover{
+        background: #215059;
+      }
+    </style>
 </head>
 <body>
     <!-- MENU -->
@@ -105,7 +113,7 @@ $produtos   = $stmt->fetchAll(PDO::FETCH_OBJ);
                         <div class="card-body" id="produtos">
                             <p class="card-title">R$<?= number_format($produto->Preco_Und, 2, ',', '.'); ?> </p>
                             <p class="card-text"><?= $produto->Nome_produto; ?> </p>
-                            <button class="btn btn-primary"><a href="./info_produto.php?id=<?= $produto->produto_ID; ?>">Comprar</a></button>
+                            <a href="./info_produto.php?id=<?= $produto->produto_ID; ?>" class="btn btn-primary form-control">Comprar</a>
                         </div>
                   </div>
             <?php endforeach; ?>
