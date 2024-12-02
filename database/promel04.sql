628c951e4089ea02718f6bdfada829b191189519
Nico-las1 <nicolasmoro013@mail.com>
2024-12-01 23:08:11 -0300

    add produtos

--- database/promel04.sql
+++ database/promel04.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 30-Nov-2024 às 18:02
--- Versão do servidor: 8.0.31
--- versão do PHP: 8.0.26
+-- Tempo de geração: 02/12/2024 às 01:47
+-- Versão do servidor: 8.2.0
+-- Versão do PHP: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,7 +26,7 @@ USE `promel04`;
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `admin`
+-- Estrutura para tabela `admin`
 --
 
 DROP TABLE IF EXISTS `admin`;
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `avaliacoes`
+-- Estrutura para tabela `avaliacoes`
 --
 
 DROP TABLE IF EXISTS `avaliacoes`;
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `avaliacoes` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `avaliacoes`
+-- Despejando dados para a tabela `avaliacoes`
 --
 
 INSERT INTO `avaliacoes` (`id_avaliacao`, `qtd_estrela`, `comentario`, `created`, `modified`) VALUES
@@ -68,7 +68,7 @@ INSERT INTO `avaliacoes` (`id_avaliacao`, `qtd_estrela`, `comentario`, `created`
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `carrinho`
+-- Estrutura para tabela `carrinho`
 --
 
 DROP TABLE IF EXISTS `carrinho`;
@@ -85,7 +85,7 @@ CREATE TABLE IF NOT EXISTS `carrinho` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `categoria`
+-- Estrutura para tabela `categoria`
 --
 
 DROP TABLE IF EXISTS `categoria`;
@@ -96,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `categoria` (
 ) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `categoria`
+-- Despejando dados para a tabela `categoria`
 --
 
 INSERT INTO `categoria` (`categoria_ID`, `Nome`) VALUES
@@ -106,7 +106,7 @@ INSERT INTO `categoria` (`categoria_ID`, `Nome`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cliente`
+-- Estrutura para tabela `cliente`
 --
 
 DROP TABLE IF EXISTS `cliente`;
@@ -119,19 +119,21 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `Info` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`cliente_ID`),
   UNIQUE KEY `UC_Email` (`Email`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `cliente`
+-- Despejando dados para a tabela `cliente`
 --
 
 INSERT INTO `cliente` (`cliente_ID`, `Email`, `Senha`, `Nome`, `Telefone`, `Info`) VALUES
-(1, 'adm@adm.com', '$2y$10$tQi2AEz5xyaSWdo1FO/D6.lK4WlwamqjIIelZia3LU4dPE8FXn8CW', 'Alex', NULL, NULL);
+(1, 'adm@adm.com', '$2y$10$tQi2AEz5xyaSWdo1FO/D6.lK4WlwamqjIIelZia3LU4dPE8FXn8CW', 'Alex', NULL, NULL),
+(2, 'vitor3@gmail.com', '$2y$10$aYaY3owARm3WeF8HI4hVWuYczeoFbJP9C6nmoVBjUQxj69CrTaTt2', 'vitor', NULL, NULL),
+(4, 'JoaoM@gmail.com', '$2y$10$oNfB8zt3aiSpscPA3RWh0.Di4fuyMZIyTLhPMHC5JBcQPAuM4roz2', 'Joao Mendez', NULL, NULL);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `pedido`
+-- Estrutura para tabela `pedido`
 --
 
 DROP TABLE IF EXISTS `pedido`;
@@ -147,7 +149,7 @@ CREATE TABLE IF NOT EXISTS `pedido` (
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `pedido`
+-- Despejando dados para a tabela `pedido`
 --
 
 INSERT INTO `pedido` (`pedido_ID`, `Qtd_produtos`, `Valor_Total`, `fk_cliente_ID`, `fk_Admin_ID`) VALUES
@@ -156,7 +158,7 @@ INSERT INTO `pedido` (`pedido_ID`, `Qtd_produtos`, `Valor_Total`, `fk_cliente_ID
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `possui`
+-- Estrutura para tabela `possui`
 --
 
 DROP TABLE IF EXISTS `possui`;
@@ -170,7 +172,7 @@ CREATE TABLE IF NOT EXISTS `possui` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `produto`
+-- Estrutura para tabela `produto`
 --
 
 DROP TABLE IF EXISTS `produto`;
@@ -187,19 +189,22 @@ CREATE TABLE IF NOT EXISTS `produto` (
   `fk_categoria_ID` int DEFAULT NULL,
   PRIMARY KEY (`produto_ID`),
   KEY `FK_produto_3` (`fk_categoria_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
--- Extraindo dados da tabela `produto`
+-- Despejando dados para a tabela `produto`
 --
 
 INSERT INTO `produto` (`produto_ID`, `Preco_Und`, `Qtd_stock`, `Qnt_vend`, `Status_prdt`, `Descricao`, `Nome_produto`, `imagem`, `imagem2`, `fk_categoria_ID`) VALUES
-(8, 8, 83478123, NULL, NULL, '2433r2w', 'oi', '674b52e4aaa8e.jpg', '674b52e4ac0a1.jpeg', 4);
+(10, 22, 5, NULL, NULL, 'Alcachofra com Berinjela', 'Alcachofra com Berinjela', '674d0ee544020.jpeg', '674d0ee54433c.jpg', 4),
+(11, 150, 15, NULL, NULL, 'Cura Tudo', 'Cura Tudo', '674d0f735c2b0.jpeg', '674d0f735d5a6.jpg', 4),
+(12, 130, 12, NULL, NULL, 'Erva Baleeira', 'Erva Baleeira', '674d10fdb654d.jpeg', '674d10fdb6900.jpg', 4),
+(13, 100, 80, NULL, NULL, 'Extrato', 'Ansie', '674d118605dbc.png', '674d118606b62.jpg', 4);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `produto_pedido_possui`
+-- Estrutura para tabela `produto_pedido_possui`
 --
 
 DROP TABLE IF EXISTS `produto_pedido_possui`;
