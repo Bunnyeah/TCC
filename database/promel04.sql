dd1cd9dba29b1fb31e334a5af48739b08a32b687
BelitMota <belitmota@mail.com>
2025-03-24 14:20:30 -0300

    Tcc Completo

--- database/promel04.sql
+++ database/promel04.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 03/12/2024 às 13:48
+-- Tempo de geração: 14/12/2024 às 17:13
 -- Versão do servidor: 8.3.0
 -- Versão do PHP: 8.2.18
 
@@ -20,21 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `promel04`
 --
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `admin`
---
-
-DROP TABLE IF EXISTS `admin`;
-CREATE TABLE IF NOT EXISTS `admin` (
-  `admin_ID` int NOT NULL AUTO_INCREMENT,
-  `Email` varchar(200) DEFAULT NULL,
-  `Senha` varchar(60) DEFAULT NULL,
-  `Telefone` varchar(60) DEFAULT NULL,
-  PRIMARY KEY (`admin_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS `promel04` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+USE `promel04`;
 
 -- --------------------------------------------------------
 
@@ -117,7 +104,7 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `Info` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`cliente_ID`),
   UNIQUE KEY `UC_Email` (`Email`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Despejando dados para a tabela `cliente`
@@ -126,47 +113,7 @@ CREATE TABLE IF NOT EXISTS `cliente` (
 INSERT INTO `cliente` (`cliente_ID`, `Email`, `Senha`, `Nome`, `Telefone`, `Info`) VALUES
 (1, 'alexpromel@gmail.com', '$2y$10$tQi2AEz5xyaSWdo1FO/D6.lK4WlwamqjIIelZia3LU4dPE8FXn8CW', 'Alex', NULL, NULL),
 (2, 'vitor3@gmail.com', '$2y$10$aYaY3owARm3WeF8HI4hVWuYczeoFbJP9C6nmoVBjUQxj69CrTaTt2', 'vitor', NULL, NULL),
-(4, 'JoaoM@gmail.com', '$2y$10$oNfB8zt3aiSpscPA3RWh0.Di4fuyMZIyTLhPMHC5JBcQPAuM4roz2', 'Joao Mendez', NULL, NULL),
-(5, 'cliente@gmail.com', '$2y$10$dQEn3MpelI3x.mL75VHxyeUgujl4UyXwRhx36xD.xPbivc9mXbR1u', 'Cliente', NULL, NULL);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `pedido`
---
-
-DROP TABLE IF EXISTS `pedido`;
-CREATE TABLE IF NOT EXISTS `pedido` (
-  `pedido_ID` int NOT NULL AUTO_INCREMENT,
-  `Qtd_produtos` int DEFAULT NULL,
-  `Valor_Total` int DEFAULT NULL,
-  `fk_cliente_ID` int DEFAULT NULL,
-  `fk_Admin_ID` int DEFAULT NULL,
-  PRIMARY KEY (`pedido_ID`),
-  KEY `FK_Pedido_2` (`fk_cliente_ID`),
-  KEY `FK_Pedido_3` (`fk_Admin_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Despejando dados para a tabela `pedido`
---
-
-INSERT INTO `pedido` (`pedido_ID`, `Qtd_produtos`, `Valor_Total`, `fk_cliente_ID`, `fk_Admin_ID`) VALUES
-(2, 2, 200, 1, 1);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `possui`
---
-
-DROP TABLE IF EXISTS `possui`;
-CREATE TABLE IF NOT EXISTS `possui` (
-  `fk_Admin_ID` int DEFAULT NULL,
-  `fk_categoria_ID` int DEFAULT NULL,
-  KEY `FK_possui_1` (`fk_Admin_ID`),
-  KEY `FK_possui_2` (`fk_categoria_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+(4, 'JoaoM@gmail.com', '$2y$10$oNfB8zt3aiSpscPA3RWh0.Di4fuyMZIyTLhPMHC5JBcQPAuM4roz2', 'Joao Mendez', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -199,22 +146,6 @@ INSERT INTO `produto` (`produto_ID`, `Preco_Und`, `Qtd_stock`, `Qnt_vend`, `Stat
 (11, 150, 14, NULL, NULL, 'Cura Tudo', 'Cura Tudo', '674d0f735c2b0.jpeg', '674d0f735d5a6.jpg', 4),
 (12, 130, 12, NULL, NULL, 'Erva Baleeira', 'Erva Baleeira', '674d10fdb654d.jpeg', '674d10fdb6900.jpg', 4),
 (13, 100, 80, NULL, NULL, 'Extrato', 'Ansie', '674d118605dbc.png', '674d118606b62.jpg', 4);
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `produto_pedido_possui`
---
-
-DROP TABLE IF EXISTS `produto_pedido_possui`;
-CREATE TABLE IF NOT EXISTS `produto_pedido_possui` (
-  `Produto_Pedido_ID` int NOT NULL AUTO_INCREMENT,
-  `fk_produto_ID` int DEFAULT NULL,
-  `fk_Pedido_ID` int DEFAULT NULL,
-  PRIMARY KEY (`Produto_Pedido_ID`),
-  KEY `FK_Produto_Pedido_possui_2` (`fk_produto_ID`),
-  KEY `FK_Produto_Pedido_possui_3` (`fk_Pedido_ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
